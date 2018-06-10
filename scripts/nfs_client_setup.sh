645f378ff9ae872d48448735166cdea0695d9404
Sher Chowdhury <sher.chowdhury@gmail.com>
2018-06-10 12:44:55 +0100

    wip

--- scripts/nfs_client_setup.sh
+++ scripts/nfs_client_setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-exit 0
+# exit 0
 set -ex
 
 echo '##########################################################################'
@@ -10,9 +10,23 @@ yum install -y nfs-utils
 
 mkdir -p /mnt/backups
 
+clienthostname=$(hostname -f)
+kadmin <<EOF
+rootpassword
+addprinc -randkey nfs/$clienthostname
+ktadd nfs/$clienthostname
+quit
+EOF
+### at this point I think if you run kadmin again, you get the following error message: 
+# [root@nfs-client rw]# kadmin
+# Authenticating as principal host/admin@CB.NET with password.
+# kadmin: Client 'host/admin@CB.NET' not found in Kerberos database while initializing kadmin interface
+### not sure why this happens. but you can override this by running the following instead. 
+# [root@nfs-client rw]# kadmin -p root/admin
 
 systemctl enable nfs-client.target
-systemctl restart nfs-client.target
+systemctl stop nfs-client.target
+systemctl start nfs-client.target
 
 # showmount -e nfs-storage.cb.net
 # mount -t nfs nfs-storage.cb.net:/nfs/export_rw /mnt/backups
